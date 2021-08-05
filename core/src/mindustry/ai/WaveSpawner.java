49d77ed3f6d28a8c485be82575ddcfd31c460c1f
JrTRinny <85090668+JrTRinny@users.noreply.github.com>
2021-08-05 21:16:31 +0700

    fix

--- core/src/mindustry/ai/WaveSpawner.java
+++ core/src/mindustry/ai/WaveSpawner.java
@@ -162,8 +162,8 @@ public class WaveSpawner{
         }
         
         //apply drop zone radius expansion when new wave is spawned
-        if(spawning && state.rules.dropZoneExpansion && (state.wave -1) > state.rules.expansionGrace){
-            Math.min(state.rules.dropZoneRadius++, state.rules.expansionPerWave);
+        if(spawning && state.rules.dropZoneExpansion && state.wave >= state.rules.expansionGrace){
+            Math.min(state.rules.dropZoneRadius++, state.rules.dropZoneRadius + state.rules.expansionPerWave);
             Math.min(state.rules.dropZoneRadius, state.rules.expansionCap);
         }
     }
