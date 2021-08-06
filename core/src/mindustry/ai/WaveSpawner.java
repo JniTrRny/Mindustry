929e05378f7c2a90eee062f956cac3efc85fb572
JrTRinny <TrinSuk123@gmail.com>
2021-08-06 13:15:23 +0700

    tryitman

--- core/src/mindustry/ai/WaveSpawner.java
+++ core/src/mindustry/ai/WaveSpawner.java
@@ -162,7 +162,7 @@ public class WaveSpawner{
         }
         
         //apply drop zone radius expansion when new wave is spawned
-        if(Time.delta > state.rules.expansionGrace && state.rules.dropZoneExpansion){
+        if(control.saves.totalPlaytime > state.rules.expansionGrace && state.rules.dropZoneExpansion){
             Mathf.lerpDelta(state.rules.dropZoneRadius, state.rules.dropZoneRadius + (state.rules.expansionPerMinute / 60f), 0.1f);
             if(state.rules.expansionCapCheck && state.rules.dropZoneRadius > state.rules.expansionCap){
                 state.rules.dropZoneRadius = state.rules.expansionCap;
